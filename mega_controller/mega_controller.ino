61a6c132b8b5a85c115624f5a6c16bff8b75aa80
Adam Johnson <adamj537@gmail.com>
2021-07-23 17:15:29 -0500

    Readability; no functional changes.

--- mega_controller/mega_controller.ino
+++ mega_controller/mega_controller.ino
@@ -8,9 +8,9 @@
 
 
 #define SPEED 850
-#define SCREW_MOTOR_PIN 4
-#define spooler_MOTOR_PIN 5
-#define FAN_PIN 6
+#define PIN_SCREW_MOTOR 4
+#define PIN_SPOOLER_MOTOR 5
+#define PIN_FAN_MOTOR 6
 
 #define FAN_SPEED 255
 
@@ -18,56 +18,67 @@
 #include "motor_ramp_up.h" 
 #include "tachometer.h" 
 
-motor_ramp_up spooler_motor;
-motor_ramp_up screw_motor;
-motor_ramp_up fan;
+// Define the motor controllers.
+motor_ramp_up motor_spooler;
+motor_ramp_up motor_screw;
+motor_ramp_up motor_fan;
 
 bool has_been_reset = false;
 
+// I2C bus to communicate with User Interface Module.
 I2C_Controller i2c_controller;
 
 void setup() {
-  Serial.begin(9600);           //  setup serial
-  Serial.println("Begin");           //  setup serial
+  // Set up serial.
+  Serial.begin(9600);
+  Serial.println("Begin");
 
-
-  screw_motor.setup(SCREW_MOTOR_PIN);
-  spooler_motor.setup(spooler_MOTOR_PIN);
+  // Set up motor drivers.
+  motor_screw.setup(PIN_SCREW_MOTOR);
+  motor_spooler.setup(PIN_SPOOLER_MOTOR);
   
-  fan.setup(FAN_PIN);
-  fan.set_speed(FAN_SPEED);
+  // Set up fans.
+  motor_fan.setup(PIN_FAN_MOTOR);
+  motor_fan.set_speed(FAN_SPEED);
 
+  // Set up tachometer.
   setup_tachometer();
   
+  // Set up I2C bus (communicates with User Interface Module).
   i2c_controller.setup();
+  
+  // Initialize global variables.
   has_been_reset = false;
 }
 
 
 void loop() {
+  // Process motor control.
+  motor_screw.loop();
+  motor_spooler.loop();
+  motor_fan.loop();
 
-  screw_motor.loop();
-  spooler_motor.loop();
-  fan.loop();
-
+  // Process tachometer.
   update_taciometer();
 
-  int desired_screw_speed = map(i2c_controller.get_desired_screw_speed(),0,1023,0,255);
+  // Get desired screw speed.
+  int desired_screw_speed = map(i2c_controller.get_desired_screw_speed(), 0, 1023, 0, 255);
+  
   if (has_been_reset){
-    screw_motor.set_speed(desired_screw_speed); 
-  }else{
-    screw_motor.set_speed(0); 
-    
+    motor_screw.set_speed(desired_screw_speed); 
   }
-  if (desired_screw_speed<4){
+  else{
+    motor_screw.set_speed(0); 
+  }
+  
+  if (desired_screw_speed < 4){
     //ensure motor is off on start up
     has_been_reset = true;
     
   }
 
-
-  int desired_spooler_speed = map(i2c_controller.get_desired_spooler_speed(),0,1023,0,255);
-  spooler_motor.set_speed(desired_spooler_speed);
+  int desired_spooler_speed = map(i2c_controller.get_desired_spooler_speed(), 0, 1023, 0, 255);
+  motor_spooler.set_speed(desired_spooler_speed);
 
   
   #if VERBOSE == 3
