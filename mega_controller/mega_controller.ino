1a581068d7d224c56750102e6b351ab5e71a58fb
Adam Johnson <adamj537@gmail.com>
2021-07-27 14:47:40 -0500

    Bug fix in motor_ramp_up. Added ability to control fan speed. Removed I2cController module (but retained functionality). Removed "VERBOSE" debug flag.

--- mega_controller/mega_controller.ino
+++ mega_controller/mega_controller.ino
@@ -1,90 +1,126 @@
-#define VERBOSE 0
-// 0 is verbose is off
-// 3 is verbose i2c verbose
-// 4 is spooler motor
-// 5 is tachometer
-// 6 is tachometer everything
-
-
-
-#define SPEED 850
-#define PIN_SCREW_MOTOR 4
-#define PIN_SPOOLER_MOTOR 5
-#define PIN_FAN_MOTOR 6
-
-#define FAN_SPEED 255
-
-#include "I2cController.h"
+#include "Wire.h"
 #include "motor_ramp_up.h" 
 #include "tachometer.h" 
 
+// Pin definitions
+static int PIN_SCREW_MOTOR = 4;
+static int PIN_SPOOLER_MOTOR = 5;
+static int PIN_FAN_MOTOR = 6;
+
+// I2C Slave Address
+static int I2C_ADDRESS = 8;
+
 // Define the motor controllers.
-motor_ramp_up motor_spooler;
-motor_ramp_up motor_screw;
-motor_ramp_up motor_fan;
+motor_ramp_up motorSpooler;
+motor_ramp_up motorScrew;
+motor_ramp_up motorFan;
 
-bool has_been_reset = false;
-
-// I2C bus to communicate with User Interface Module.
-I2C_Controller i2c_controller;
+bool screwReset;
+int motorSpeedScrew;		// desired screw motor speed
+int motorSpeedSpooler;		// desired spooler motor speed
+int motorSpeedFan;			// desired fan motor speed
 
 void setup() {
+  // Initialize global variables.
+  motorSpeedScrew = 0;
+  motorSpeedSpooler = 0;
+  motorSpeedFan = 255;
+  screwReset = false;
+  
   // Set up serial.
   Serial.begin(9600);
   Serial.println("Begin");
-
-  // Set up motor drivers.
-  motor_screw.setup(PIN_SCREW_MOTOR);
-  motor_spooler.setup(PIN_SPOOLER_MOTOR);
   
-  // Set up fans.
-  motor_fan.setup(PIN_FAN_MOTOR);
-  motor_fan.set_speed(FAN_SPEED);
+  // Set up motor drivers.
+  motorScrew.setup(PIN_SCREW_MOTOR, 3, 5);
+  motorSpooler.setup(PIN_SPOOLER_MOTOR, 3, 5);
+  motorFan.setup(PIN_FAN_MOTOR, 3, 5);
 
   // Set up tachometer.
   setup_tachometer();
   
   // Set up I2C bus (communicates with User Interface Module).
-  i2c_controller.setup();
-  
-  // Initialize global variables.
-  has_been_reset = false;
+  Wire.begin(I2C_ADDRESS);
+  Wire.onReceive(receiveEvent);
 }
 
 
 void loop() {
   // Process motor control.
-  motor_screw.loop();
-  motor_spooler.loop();
-  motor_fan.loop();
+  motorScrew.loop();
+  motorSpooler.loop();
+  motorFan.loop();
 
   // Process tachometer.
   update_taciometer();
 
-  // Get desired screw speed.
-  int desired_screw_speed = map(i2c_controller.get_desired_screw_speed(), 0, 1023, 0, 255);
+  // Update screw speed.
+  int desired_screw_speed = map(motorSpeedScrew, 0, 1023, 0, 255);
   
-  if (has_been_reset){
-    motor_screw.set_speed(desired_screw_speed); 
+  if (screwReset) {
+    motorScrew.set_speed(desired_screw_speed); 
   }
   else{
-    motor_screw.set_speed(0); 
+    motorScrew.set_speed(0); 
   }
   
-  if (desired_screw_speed < 4){
+  if (desired_screw_speed < 4) {
     //ensure motor is off on start up
-    has_been_reset = true;
-    
+    screwReset = true;
   }
 
-  int desired_spooler_speed = map(i2c_controller.get_desired_spooler_speed(), 0, 1023, 0, 255);
-  motor_spooler.set_speed(desired_spooler_speed);
+  // Update spooler speed.
+  int desired_spooler_speed = map(motorSpeedSpooler, 0, 1023, 0, 255);
+  motorSpooler.set_speed(desired_spooler_speed);
 
+  // Update fan speed.
+  int desiredFanSpeed = map(motorSpeedFan, 0, 1023, 0, 255);
+  motorFan.set_speed(desiredFanSpeed);
   
-  #if VERBOSE == 3
-    Serial.print("I2C: ");
-    Serial.print(i2c_controller.get_desired_screw_speed());
-    Serial.print(" ");
-    Serial.println(i2c_controller.get_desired_spooler_speed());
-  #endif
+  //Serial.print("Screw speed: ");
+  //Serial.print(motorSpeedScrew);
+  //Serial.print(" ");
+  //Serial.print("Spooler speed: ");
+  //Serial.print(motorSpeedSpooler);
+  //Serial.print(" ");
+  //Serial.print("Fan speed: ");
+  //Serial.println(motorSpeedFan);
+}
+
+// Read incoming I2C data from UI Module.
+void receiveEvent(int numBytes) {
+  byte c1 = 0;	// received byte
+
+  // For each byte received...
+  for(int j = 0; j < numBytes; j++) {
+	// Read the byte.
+    byte data = Wire.read();
+
+	// The first two bytes are the desired screw motor speed.
+    if (j == 0) {
+       c1 = data;
+    }
+
+    if (j == 1) {
+       motorSpeedScrew = (int)(c1 << 8) + (int) data;
+    }
+
+	// The 2nd two bytes are the desired spooler motor speed.
+    if (j == 2) {
+       c1 = data;
+    }
+
+    if (j == 3) {
+       motorSpeedSpooler = (int)(c1 << 8) + (int) data;
+    }
+	
+	// The 3rd two bytes are the desired fan speed.
+	if (j == 4) {
+		c1 = data;
+	}
+	
+	if (j == 5) {
+		motorSpeedFan = (int)(c1 << 8) + (int) data;
+	}
+  }
 }
--- mega_controller/motor_ramp_up.h
+++ mega_controller/motor_ramp_up.h
@@ -1,71 +1,62 @@
 /*
- * 
  * This code will slowly increase the speed of the motors 
  * so that these is not too high a current throught the system
- * 
  */
+
 #ifndef MOTOR_RAMP_UP_H
 #define MOTOR_RAMP_UP_H
 
 #include "Arduino.h"
 
-#define STEP_VALUE 3
-
 class motor_ramp_up
 {
 private:
-    /* data */
-    unsigned long last_loop_time;
-    unsigned long ramp_up_ms;
-    unsigned int ramp_step;
-    int goal_speed;
-    int speed;
-    int change;
-    int motor_pin = 0;
+	int motorPin = 0;
+    unsigned long timestamp;
+    unsigned long motorRampTime;
+    unsigned int motorRampStep;
+    byte speedCurrent;
+    byte speedGoal;
 public:
     motor_ramp_up();
-    // define the pins. The ramp function will move `new_ramp_step` every `new_ramp_up_ms`
-    void motor_ramp_up::setup(int new_motor_pin, int new_ramp_up_ms, int new_ramp_step);
-    void motor_ramp_up::setup(int new_motor_pin);
-    void loop();//call every loop
-    int get_speed(){return speed;}//get the current speed
-    void set_speed(int new_speed);//set the desired speed
-
+	
+    // Define motor pins and ramp speed. The motor's output will change by
+	// 'rampStep' counts every `rampTime' ms.
+    void setup(int pin, int rampTime, int rampStep);
+	
+	// Process motor speed.  Call in main loop.
+    void loop();
+	
+	// Get the current speed.
+    byte get_speed();
+	
+	// Set the desired speed.
+    void set_speed(byte speed);
 };
 
-
-
-/*
- Example
-
-#define MOTOR_PIN 11
+/* Example
 
 #include "motor_ramp_up.h"
 
-motor_ramp_up spooler_motor;
+const int PIN_MOTOR = 11;
 
-
+motor_ramp_up motor;
 
 void setup() {
-
-  spooler_motor.setup(MOTOR_PIN,1000);
+  motor.setup(MOTOR_PIN, 3, 5);
 }
-
 
 void loop() {
-
-  spooler_motor.loop();
+  motor.loop();
 
   if (TURN MOTOR ON){
-    spooler_motor.set_speed(1023);
+    motor.set_speed(1023);
   }
   else if (TURN MOTOR OFF){
-    spooler_motor.set_speed(0);
+    motor.set_speed(0);
   }
-
-
 }
   
- */
+*/
 
 #endif
