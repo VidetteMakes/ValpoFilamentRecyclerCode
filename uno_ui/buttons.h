0d76a122892d80d7a07c1730f3ba706418a404e8
Adam Johnson <adamj537@gmail.com>
2021-07-28 16:18:03 -0500

    Removed menu; just display motor speeds.

--- uno_ui/buttons.h
+++ uno_ui/buttons.h
@@ -1,64 +1,21 @@
 #ifndef BUTTONS
 #define BUTTONS
 
-#include "Wire.h"
-#include "Adafruit_LiquidCrystal.h"
-
-// 0 = main menu
-// 1 = monitor temp and diameter
-// 2 = select from presets
-// 3 = manual adjustment of screw speed
-// 4 = display preset instructions 
-int UIState = 0;
-
-// tracks position of "cursor" when selecting menu options
-int menuScroll;
-
-// digital pinout for input buttons
-const int PIN_BUTTON_UP = 11;
-const int PIN_BUTTON_LEFT = 6;
-const int PIN_BUTTON_DOWN = 4;
-const int PIN_BUTTON_RIGHT = 8;
-const int PIN_POT_LEFT = 0;		// left potentiometer
-const int PIN_POT_RIGHT = 1;	// right potentiometer
-
-// left potentiometer's value
-float valPotL = 0;
-
-// right potentiometer's value
-float valPotR = 0;
+const int PIN_BUTTON_UP = 11;			// pin for "up" button
+const int PIN_BUTTON_LEFT = 6;			// pin for "left" button
+const int PIN_BUTTON_DOWN = 4;			// pin for "down" button
+const int PIN_BUTTON_RIGHT = 8;			// pin for "right" button
 
 // button debouncing variables
-int buttonStateLeft;						// the current reading from the input pin
-int lastButtonStateLeft = LOW;				// the previous reading from the input pin
-int buttonStateBot;							// the current reading from the input pin
-int lastButtonStateBot = LOW;				// the previous reading from the input pin
-unsigned long lastDebounceTimeLeft = 0;		// the last time the output pin was toggled
-unsigned long lastDebounceTimeBot = 0;		// the last time the output pin was toggled
-unsigned long debounceDelay = 50;			// the debounce time; increase if the output flickers
+int buttonStateLeft;					// the current reading from the input pin
+int lastButtonStateLeft = LOW;			// the previous reading from the input pin
+int buttonStateBot;						// the current reading from the input pin
+int lastButtonStateBot = LOW;			// the previous reading from the input pin
+unsigned long lastDebounceTimeLeft = 0;	// the last time the output pin was toggled
+unsigned long lastDebounceTimeBot = 0;	// the last time the output pin was toggled
+unsigned long debounceDelay = 50;		// the debounce time; increase if the output flickers
 
-byte msb;									// diameter most significant byte
-byte lsb;									// diameter least significant byte
-short tempD;								// diameter reading
-float diameterIn;							// diameter reading from dial indicator
-static int speedOutScrew = 0;				// desired screw motor speed
-static int speedOutspooler = 0;				// desired spooler motor speed
-
-#define TEMP_PLA 200
-#define SPEED_PLA 14
-
-// Connect via i2c, default address #0 (A0-A2 not jumpered)
-Adafruit_LiquidCrystal lcd(0);
-
-void buttonSetup(){
-	// Set up the LCD's number of rows and columns.
-	lcd.begin(16, 2);
-
-	// Initialize the pushbutton pins as inputs with pullup resistors.
-	pinMode(PIN_BUTTON_LEFT, INPUT_PULLUP);
-	pinMode(PIN_BUTTON_DOWN, INPUT_PULLUP);
-}
-
+// TODO:  Change this into something that can be called for multiple buttons.
 void checkButton(){
 	/***********************************LEFT BUTTON**************************************/
 
@@ -84,8 +41,7 @@ void checkButton(){
 
 			// If the new button state is HIGH...
 			if (buttonStateLeft == HIGH) {
-				UIState = 0;
-				lcd.clear();
+				// TODO:  Toggle fans on and off.
 			}
 		}
 	}
@@ -117,183 +73,13 @@ void checkButton(){
 
 			// If the new button state is HIGH...
 			if (buttonStateBot == HIGH) {
-				switch (UIState){
-				// main menu
-				case 0:
-					switch (menuScroll){
-					case 3:
-						// lcd.print("[Prst] Mnl  Mon ");
-						UIState = 2;
-						break;
-					case 2:
-						// lcd.print(" Prst [Mnl] Mon ");
-						UIState = 3;
-						break;
-					case 1:
-						// lcd.print(" Prst  Mnl [Mon]");
-						UIState = 1;
-						break;
-					}
-					break;
-				// monitor
-				case 1:
-					break;
-				// presets
-				case 2:
-					switch (menuScroll){
-					case 2:
-						// lcd.print("[PLA] PETG ");
-						UIState = 4;
-						break;
-					case 1:
-						// lcd.print(" PLA [PETG]");
-						UIState = 4;
-						break;
-					}
-					break;
-				case 3: // manual control
-					//Wire.beginTransmission(4);         // transmit to device 8
-					//Wire.write(speedOutScrew);         // sends one byte
-					//Wire.endTransmission();            // stop transmitting
-
-					delay(100);
-					break;
-				}
-				lcd.clear();
+				// TODO:  Something???
 			}
 		}
 	}
 
 	// Save the reading. Next time through the loop, it'll be the lastButtonState:
 	lastButtonStateBot = readingBot;
-}
-
-int getScrewSpeed() {
-	return speedOutScrew;
-}
-
-int getspoolerSpeed() {
-	return speedOutspooler;
-}
-
-void UILoop() {
-	valPotL = analogRead(PIN_POT_LEFT);    // read the value from the sensor
-	valPotR = analogRead(PIN_POT_RIGHT);    // read the value from the sensor
-
-	//not sure about this
-	speedOutScrew = valPotL;//map(valPotL,0,1023,255,0);
-	speedOutspooler = valPotR;//map(valPotR,0,1023,255,0);
-
-	switch (UIState){
-	// Main Menu
-	case 0:
-		lcd.setCursor(0, 0);
-		lcd.print("MENU");
-
-		menuScroll = 4*(valPotR/1023);
-		lcd.setCursor(0, 1);
-
-		switch (menuScroll){
-		case 3:
-			lcd.print("[Prst] Mnl  Mon ");
-			break;
-		case 2:
-			lcd.print(" Prst [Mnl] Mon ");
-			break;
-		case 1:
-			lcd.print(" Prst  Mnl [Mon]");
-		break;
-		default:
-			lcd.print(" Prst  Mnl  Mon ");
-			break;
-		}
-		break;
-	// Monitor diameter and temperature.
-	case 1:
-
-		lcd.setCursor(0, 0);
-
-		// Request 2 bytes from slave device at address 8.
-		Wire.requestFrom(8, 2);
-
-		// Slave may send less than requested.
-		while (Wire.available()) {
-			// Receive a byte as character.
-			msb = Wire.read();
-			lsb = Wire.read();
-			tempD = msb;
-			tempD = tempD << 8 | lsb;
-			diameterIn = float(tempD) / 1000;
-		}
-		delay(500);
-
-		//Serial.println(tempD);
-
-		lcd.print("DIAM: ");
-		lcd.print(diameterIn,3);
-		lcd.print(" mm   ");
-		// lcd.print(" RPM");
-
-		break;
-	// select presets for PLA and PETG (not implemented)
-	case 2:
-		lcd.setCursor(0, 0);
-		lcd.print("PRESETS");
-
-		menuScroll = 3*(valPotR/1023);
-		lcd.setCursor(0, 1);
-
-		switch (menuScroll){
-		case 2:
-			lcd.print("[PLA] PETG ");
-			break;
-		case 1:
-			lcd.print(" PLA [PETG]");
-			break;
-		default:
-			lcd.print(" PLA  PETG ");
-			break;
-		}
-		break;
-	// manually adjust speed and temperature
-	case 3:
-		lcd.setCursor(0, 0);
-
-		speedOutScrew = valPotL;//map(valPotL,0,1023,255,0);
-		speedOutspooler = valPotR;//map(valPotR,0,1023,255,0);
-		lcd.print("SPEED: ");
-		if(speedOutScrew < 10){
-			lcd.print(" ");
-		}
-		lcd.print(speedOutScrew);
-		lcd.print(" RPM");
-
-		break;
-	// display preset instructions
-	case 4:
-		lcd.setCursor(0,0);
-		switch (menuScroll){
-		case 2:
-			// lcd.print("[PLA] PETG ");
-			lcd.print("SET TEMP: ");
-			lcd.print(TEMP_PLA);
-			lcd.print(" C");
-			lcd.setCursor(0,1);
-			lcd.print("SET SPD: ");
-			lcd.print(SPEED_PLA);
-			lcd.print(" RPM");
-			break;
-		case 1:
-			// lcd.print(" PLA [PETG]");
-			lcd.print("NOT YET");
-			lcd.setCursor(0,1);
-			lcd.print("IMPLEMENTED");
-			break;
-		}
-		break;
-	}
-
-	checkButton();
 }
 
 #endif
--- uno_ui/uno_ui.ino
+++ uno_ui/uno_ui.ino
@@ -3,13 +3,25 @@
 #include "Adafruit_LiquidCrystal.h"
 #include "buttons.h"
 
-#define CONTROLLER_UI_SLAVE 8
+const int I2C_ADDR_CONTROLLER = 8;		// address of controller Arduino
+const int PIN_POT_LEFT = 0;				// left potentiometer
+const int PIN_POT_RIGHT = 1;			// right potentiometer
 
-float potVal = 0;
-int motorVal = 0;
+float valPotL = 0;						// left potentiometer's value
+float valPotR = 0;						// right potentiometer's value
+static int speedScrew = 0;				// desired screw motor speed
+static int speedSpooler = 0;			// desired spooler motor speed
+
+// Connect via i2c, default address #0 (A0-A2 not jumpered)
+Adafruit_LiquidCrystal lcd(0);
 
 void setup() {
-	buttonSetup();
+	// Set up the LCD's number of rows and columns.
+	lcd.begin(16, 2);
+
+	// Initialize the pushbutton pins as inputs with pullup resistors.
+	pinMode(PIN_BUTTON_LEFT, INPUT_PULLUP);
+	pinMode(PIN_BUTTON_DOWN, INPUT_PULLUP);
 
 	// Set up I2C (as master).
 	Wire.begin();
@@ -19,28 +31,51 @@ void setup() {
 }
 
 void loop() {
-	Serial.print(getScrewSpeed());
+	Serial.print(speedScrew);
 	Serial.print(" ");
-	Serial.println(getspoolerSpeed());
+	Serial.println(speedSpooler);
 
-	UILoop();
+	// Read potentiometer values.
+	valPotL = analogRead(PIN_POT_LEFT);
+	valPotR = analogRead(PIN_POT_RIGHT);
 
+	// Convert to PWM values.
+	speedScrew = valPotL;//map(valPotL,0,1023,255,0);
+	speedSpooler = valPotR;//map(valPotR,0,1023,255,0);
+
+	// Print settings on LCD display.
+	lcd.setCursor(0, 0);
+	lcd.print("SPEED: ");
+	if(speedScrew < 10){
+		lcd.print(" ");
+	}
+	lcd.print(speedScrew);
+	lcd.print(" RPM");
+
+	checkButton();
+	
 	sendI2C();
 }
 
 void sendI2C(){
-	Wire.beginTransmission(8);         // transmit to device 8
-	int data = getScrewSpeed();
+	Wire.beginTransmission(I2C_ADDR_CONTROLLER);
+	
+	// Send the desired screw speed.
+	int data = speedScrew;
 	char c1 = data >> 8;
 	char c2 = data & 0x00ff;
 	Wire.write(c1);              // sends one byte
 	Wire.write(c2);              // sends one byte
 
-	data = getspoolerSpeed();
+	// Send the desired spooler speed.
+	data = speedSpooler;
 	c1 = data >> 8;
 	c2 = data & 0x00ff;
 	Wire.write(c1);              // sends one byte
 	Wire.write(c2);              // sends one byte
+	
+	// Send the desired fan speed.
+	data = 
 
 	Wire.endTransmission();            // stop transmitting 
 }
