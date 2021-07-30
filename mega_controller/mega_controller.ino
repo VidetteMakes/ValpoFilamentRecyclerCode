75fc7876de0ae70c5befb03feb2d1c4fbfb7780c
Adam Johnson <adamj537@gmail.com>
2021-07-30 14:23:12 -0500

    Removed (unused) code references to motor_ramp_up module.

--- mega_controller/mega_controller.ino
+++ mega_controller/mega_controller.ino
@@ -1,27 +1,21 @@
 #include "Wire.h"
-#include "motor_ramp_up.h" 
 
 // Pin definitions
 static int PIN_SCREW_MOTOR = 4;
-static int PIN_SPOOLER_MOTOR = 5;
+static int PIN_SPOOL_MOTOR = 5;
 static int PIN_FAN_MOTOR = 6;
 
 // I2C Slave Address
 static int I2C_ADDRESS = 8;
 
-// Define the motor controllers.
-motor_ramp_up motorSpooler;
-motor_ramp_up motorScrew;
-motor_ramp_up motorFan;
-
 int motorSpeedScrew;		// desired screw motor speed
-int motorSpeedSpooler;		// desired spooler motor speed
+int motorSpeedSpool;		// desired spool motor speed
 int motorSpeedFan;			// desired fan motor speed
 
 void setup() {
   // Initialize global variables.
   motorSpeedScrew = 0;
-  motorSpeedSpooler = 0;
+  motorSpeedSpool = 0;
   motorSpeedFan = 1023;
   
   // Set up serial.
@@ -29,13 +23,8 @@ void setup() {
 
   // Set motor control pins as output.
   pinMode(PIN_SCREW_MOTOR, OUTPUT);
-  pinMode(PIN_SPOOLER_MOTOR, OUTPUT);
+  pinMode(PIN_SPOOL_MOTOR, OUTPUT);
   pinMode(PIN_FAN_MOTOR, OUTPUT);
-  
-  // Set up motor drivers.
-//  motorScrew.setup(PIN_SCREW_MOTOR, 3, 5);
-//  motorSpooler.setup(PIN_SPOOLER_MOTOR, 3, 5);
-//  motorFan.setup(PIN_FAN_MOTOR, 3, 5);
   
   // Set up I2C bus (as slave).
   Wire.begin(I2C_ADDRESS);
@@ -44,29 +33,21 @@ void setup() {
 
 
 void loop() {
-  // Process motor control.
-  motorScrew.loop();
-  motorSpooler.loop();
-  motorFan.loop();
-
   // Update screw speed.
   int desired_screw_speed = map(motorSpeedScrew, 0, 1023, 0, 255);
-//motorScrew.set_speed(desired_screw_speed);
-	analogWrite(PIN_SCREW_MOTOR, desired_screw_speed);
+  analogWrite(PIN_SCREW_MOTOR, desired_screw_speed);
 
   // Update spooler speed.
-  int desired_spooler_speed = map(motorSpeedSpooler, 0, 1023, 0, 255);
-//motorSpooler.set_speed(desired_spooler_speed);
-  analogWrite(PIN_SPOOLER_MOTOR, desired_spooler_speed);
+  int desired_spooler_speed = map(motorSpeedSpool, 0, 1023, 0, 255);
+  analogWrite(PIN_SPOOL_MOTOR, desired_spooler_speed);
 
   // Update fan speed.
   int desiredFanSpeed = map(motorSpeedFan, 0, 1023, 0, 255);
-//motorFan.set_speed(desiredFanSpeed);
   analogWrite(PIN_FAN_MOTOR, desiredFanSpeed);
 
   Serial.print(motorSpeedScrew);
   Serial.print(" ");
-  Serial.print(motorSpeedSpooler);
+  Serial.print(motorSpeedSpool);
   Serial.print(" ");
   Serial.println(motorSpeedFan);
 
@@ -97,7 +78,7 @@ void receiveEvent(int numBytes) {
     }
 
     if (j == 3) {
-       motorSpeedSpooler = (int)(c1 << 8) + (int) data;
+       motorSpeedSpool = (int)(c1 << 8) + (int) data;
     }
 	
     // The 3rd two bytes are the desired fan speed.
